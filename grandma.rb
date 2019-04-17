3a5698bb9ed0e17131188b67d5cf796a1c525967
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-17 15:02:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(speak)
   
   if speak == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif speak.capitalize == speak
+  elsif speak.capitalize === speak
   "NO, NOT SINCE 1938!"
   else
    "HUH?! SPEAK UP, SONNY!"
