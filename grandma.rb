13c89cee851ecac5c1a7f4db4c1eef75ff9836d6
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-17 14:56:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,8 @@ def speak_to_grandma
   
   if speak == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-elsif
-  
- else 
-   "HUH?! SPEAK UP, SONNY!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
@@ -20,5 +18,3 @@ elsif
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
- end
-end
